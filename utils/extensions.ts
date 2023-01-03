50d17c68e817785f23746252ad4a4f5a06706fa3
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2023-01-03 13:32:44 -0600

    Remove JSON extension

--- utils/extensions.ts
+++ utils/extensions.ts
@@ -1,5 +1,5 @@
 const extensions = [
-  '.js', '.jsx', '.ts', '.tsx', '.json', '.mjs', '.cjs', '.vue', '.astro',
+  '.js', '.jsx', '.ts', '.tsx', '.mjs', '.cjs', '.vue', '.astro',
   '.css', '.less', '.sass', '.scss', '.styl', '.html', '.htm', '.xml', '.svg',
   '.svelte', '.md', '.mdx', '.yaml', '.yml', '.toml',
 ]
